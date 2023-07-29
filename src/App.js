d1893626e3146bbc45df2e42e942de4542f91413
ranggo pato <patoranggo@gmail.com>
2023-07-29 23:14:11 +0700

    faq

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { items } from "./data/items";
+import { GoChevronDown, GoChevronLeft } from "react-icons/go";
 
 function App() {
+  const [expandedIndex, setExpandedIndex] = useState(-1);
+  const handleClick = (nextIndex) => {
+    setExpandedIndex((current) => {
+      if (current === nextIndex) {
+        return -1;
+      }
+      return nextIndex;
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className="content">
+        <img src="/faq.jpg" alt="FAQ Image" width={567} height={425} />
+        <div className="questions">
+          <div className="title">Frequently Asked Questions</div>
+          <div className="accodions">
+            {items.map((item, index) => {
+              const isExpanded = index === expandedIndex;
+              const icon = isExpanded ? <GoChevronDown /> : <GoChevronLeft />;
+              return (
+                <div className="item">
+                  <div className="header" onClick={() => handleClick(index)}>
+                    <div className="label">{item.label}</div>
+                    <div className="icon">{icon}</div>
+                  </div>
+                  {isExpanded && <div className="text">{item.text}</div>}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/data/items.js
@@ -0,0 +1,33 @@
+export const items = [
+  {
+    id: "l2kj5",
+    label: "Apa itu channel mister dacoding channel?",
+    text: "Mister dacoding channel adalah channel yang mengajarkan tutorial programming kepada masyarakat indonesia dengan gratis di youtube, dengan content yang insyaallah menarik dan dapat menjadi pelopor perkembangan IT di indonesia",
+  },
+  {
+    id: "lk2j35lkj",
+    label: "Kenapa membuat channel pemograman atau coding channel?",
+    text: "Karena ketika saya pertama kali belajar sangat sulit menemukan channel dan modul pembelajaran secara gratis yang membantu meningkatkan skill bukan hanya sebatas content creator saja",
+  },
+  {
+    id: "l1kj2i0g",
+    label: "Apa tujuan membuat youtube pemogramana?",
+    text: "Tujuan utama saya adalah untuk memberikan modul ataupun materi terkait pemograman secara free tanpa harus mengikuti bootcamp, sehingga bisa meningkatkan kecerdasan bangsa. Aseeek",
+  },
+  {
+    id: "kjfhdg9",
+    label: "Apa kontent / project pemograman yang saat ini sedang disiapkan?",
+    text: "Untuk saat ini saya berfokus memberikan konten untuk framework JS yaitu Reactjs, setelah itu saya akan pindah ke Angular",
+  },
+  {
+    id: "dsoing9",
+    label:
+      "Apakah akan ada Tutorial React untuk pemula dari awal sampai mahir?",
+    text: "Tentu saja, Materinya sedang disiapkan, kita akan membahas semua tentang react baik jsx, props, state, hooks, routing, context, integration api, CRUD, modal, berbagai project, redux terbaru yaitu redux toolkit, dan cara desain state ataupun event dalam suatu project",
+  },
+  {
+    id: "nmhcj9",
+    label: "Apa harapan mister dacoding kedepan?",
+    text: "Harapannya adalah mister dacoding channel bisa menjadi pelopor berkembangnya skill dan karir setiap orang yang ingin belajar pemograman secara otodidak tanpa bayaran apapun",
+  },
+];
